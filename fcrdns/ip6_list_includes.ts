19d4b80ed041641398d282baaa43dae1e934a3e2
James Bradlee <james@nslo.no>
2025-08-23 00:03:12 +0200

    refactor: remove unnecessary JSDoc comment for ip6ListIncludes function

--- fcrdns/ip6_list_includes.ts
+++ fcrdns/ip6_list_includes.ts
@@ -1,9 +1,6 @@
 import { arrayEq } from "./array_eq.ts";
 import { parseIPv6 } from "./parse_ipv6.ts";
 
-/**
- * @param ip Assumed to be an expanded IPv6 address.
- */
 export function ip6ListIncludes(
   list: string[],
   ip: Uint8Array,
